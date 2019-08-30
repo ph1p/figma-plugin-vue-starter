b2dedc3eb2e07ebef4eb44be5c697c3e8ea0cee6
ph1p <phil@capshake.com>
2019-08-30 15:52:41 +0200

    refactor: better develop experience

--- vue.config.js
+++ vue.config.js
@@ -1,44 +1,73 @@
-let webpack = require('webpack');
-let HtmlWebpackPlugin = require('html-webpack-plugin');
-let HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
 
-module.exports = {
-  css: {
-    extract: false
-  },
-  configureWebpack: {
-    devtool: false,
-    optimization: {
-      splitChunks: false
-    },
-    entry: {
-      app: './src/ui/main.ts'
-    },
-    resolve: {
-      alias: {
-        '@': __dirname + '/src/ui'
+const isMain = process.env.TARGET_NODE === 'main';
+
+if (isMain) {
+  webpack(
+    {
+      stats: 'minimal',
+      mode: 'production',
+      devtool: false,
+      entry: {
+        main: './src/main/index.ts'
+      },
+      module: {
+        rules: [{ test: /\.ts?$/, use: 'ts-loader', exclude: /node_modules/ }]
+      },
+      resolve: { extensions: ['.tsx', '.ts', '.jsx', '.js'] },
+      output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist')
       }
     },
-    output: {
-      filename: '[name].js',
-      chunkFilename: '[name].js'
+    () => {
+      console.log('main created');
+    }
+  );
+} else {
+  const config = {
+    css: {
+      extract: false
     },
-    devServer: {
-      writeToDisk: true
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: 'public/index.html',
-        inlineSource: '.(js|css)$',
-        chunks: ['app']
-      }),
-      new HtmlWebpackInlineSourcePlugin(HtmlWebpackPlugin)
-    ]
-  },
-  chainWebpack: config => {
-    config
-      .entry('main')
-      .add('./src/main/index.ts')
-      .end();
-  }
-};
+    configureWebpack: {
+      stats: 'minimal',
+      devtool: false,
+      optimization: {
+        splitChunks: false
+      },
+      entry: {
+        app: './src/ui/main.ts'
+      },
+      resolve: {
+        alias: {
+          '@': __dirname + '/src/ui'
+        }
+      },
+      output: {
+        filename: '[name].js',
+        chunkFilename: '[name].js'
+      },
+      devServer: {
+        host: '127.0.0.1',
+        port: 8080,
+        compress: true,
+        disableHostCheck: true,
+        writeToDisk: true,
+        historyApiFallback: true
+      },
+      plugins: [
+        new HtmlWebpackPlugin({
+          template: 'public/index.html',
+          inlineSource: '.(js|css)$',
+          chunks: ['app']
+        }),
+        new HtmlWebpackInlineSourcePlugin(HtmlWebpackPlugin)
+      ]
+    }
+  };
+
+  module.exports = config;
+}
